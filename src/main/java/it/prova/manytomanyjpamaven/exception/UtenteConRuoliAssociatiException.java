c93944c924467630842526420e296c0ef29e1091
Saverio <save.carelli@gmail.com>
2022-05-23 14:21:43 +0200

    primo commit

---
+++ src/main/java/it/prova/manytomanyjpamaven/exception/UtenteConRuoliAssociatiException.java
@@ -0,0 +1,13 @@
+package it.prova.manytomanyjpamaven.exception;
+
+public class UtenteConRuoliAssociatiException extends Exception {
+
+	public UtenteConRuoliAssociatiException() {
+	}
+
+	public UtenteConRuoliAssociatiException(String message) {
+		super(message);
+
+	}
+
+}
